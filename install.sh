5c40748512e037a66e70cd0dea1d662f0de9c00a
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-27 02:48:19 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+DISK="/dev/vda"
+
+# BIOS
+printf "o\nn\n\n\n\n+8G\nn\n\n\n\n\nt\n1\n82\na\n2\nw\n" | fdisk $DISK  
+mkswap "$DISK""1"
+mkfs.ext4 "$DISK""2"
+mount "$DISK""2" /mnt
+swapon "$DISK""1"
+
+pacstrap -K /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+cd /mnt
+wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch/main/after-chroot.sh
+chmod +x after-chroot.sh
+arch-chroot /mnt
+./after-chroot.sh
