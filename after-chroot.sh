1505f569c7b1c689cdbc0298c6fe887a978092a4
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-27 03:09:19 +1000

    Create after-chroot.sh

---
+++ after-chroot.sh
@@ -0,0 +1,32 @@
+#DISK="/dev/sda"
+DISK="/dev/vda"
+
+
+ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "arch" > /etc/hostname
+
+pacman -S --noconfirm networkmanager neovim sudo python-pip base-devel os-prober grub vlc htop git wget
+pacman -S --noconfirm xorg xclip
+
+systemctl enable NetworkManager
+
+printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=9 nowatchdog nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+grub-install $DISK # BIOS
+# grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# For my hard drive
+printf "[defaults]\nntfs_defaults=uid=\$UID,gid=\$GID\n" > /etc/udisks2/mount_options.conf
+
+useradd -m -G users,wheel,audio,video,sddm -s /bin/bash connor
+printf " \n \n" | passwd
+printf " \n \n" | passwd connor
+echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) NOPASSWD: ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
+
+cd /home/connor
+wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch/main/user.sh
+chmod +x user.sh
