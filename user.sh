ac7c396bfbac6590fb01c499098aa9185622a486
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-29 18:18:27 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -19,6 +19,11 @@ cd /home/connor/suckless/dmenu && sudo make clean install
 printf "exec dwm" > /home/connor/.xsession # needed to boot with lightdm
 chmod +x /home/connor/.xsession
 
+# Packer
+# https://github.com/wbthomason/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
 paru -S --noconfirm  noto-fonts noto-fonts-cjk noto-fonts-emoji
 paru -S --noconfirm google-chrome onlyoffice-bin discord obsidian flameshot alacritty
 
