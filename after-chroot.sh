88ef1cba450221e3c35476a79e3bb61400600e44
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-29 17:41:04 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -39,19 +39,6 @@ printf " \n \n" | passwd
 printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) NOPASSWD: ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
 
-# suckless
-mkdir /home/connor/suckless
-cd /home/connor/suckless
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
-git clone https://git.suckless.org/dmenu
-cd /home/connor/suckless/dwm && sudo make clean install
-cd /home/connor/suckless/st && sudo make clean install
-cd /home/connor/suckless/dmenu && sudo make clean install
-#printf "exec dwm" > /home/connor/.xinitrc # technically not needed, it's just to be able to run 'startx'
-printf "exec dwm" > /home/connor/.xsession # needed to boot with lightdm
-chmod +x /home/connor/.xsession
-
 cd /home/connor
 wget https://raw.githubusercontent.com/Connor-McCartney/deploy-arch-dwm/refs/heads/main/user.sh
 chmod +x user.sh
