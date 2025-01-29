b644475e1c7b0268fcbf3b0e991adebea79150ff
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-29 17:41:14 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -6,5 +6,17 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si --noconfirm
 
+# suckless
+mkdir /home/connor/suckless
+cd /home/connor/suckless
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dmenu
+cd /home/connor/suckless/dwm && sudo make clean install
+cd /home/connor/suckless/st && sudo make clean install
+cd /home/connor/suckless/dmenu && sudo make clean install
+#printf "exec dwm" > /home/connor/.xinitrc # technically not needed, it's just to be able to run 'startx'
+printf "exec dwm" > /home/connor/.xsession # needed to boot with lightdm
+chmod +x /home/connor/.xsession
 
 rm /home/connor/user.sh
