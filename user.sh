ee39f50c2e133e4a447077f6f0c01e5b2da78697
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-01-31 02:12:04 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -7,15 +7,16 @@ cd paru-bin
 makepkg -si --noconfirm
 
 # suckless
-mkdir /home/connor/suckless
-cd /home/connor/suckless
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
-git clone https://git.suckless.org/dmenu
+#mkdir /home/connor/suckless
+#cd /home/connor/suckless
+#git clone https://git.suckless.org/dwm
+#git clone https://git.suckless.org/st
+#git clone https://git.suckless.org/dmenu
 cd /home/connor/suckless/dwm && sudo make clean install
-cd /home/connor/suckless/st && sudo make clean install
 cd /home/connor/suckless/dmenu && sudo make clean install
+#cd /home/connor/suckless/st && sudo make clean install
 #printf "exec dwm" > /home/connor/.xinitrc # technically not needed, it's just to be able to run 'startx'
+
 printf "exec dwm" > /home/connor/.xsession # needed to boot with lightdm
 chmod +x /home/connor/.xsession
 
