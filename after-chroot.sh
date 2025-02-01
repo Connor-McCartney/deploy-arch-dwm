5bd8882ca76271e81f6af20beea6e994a6d2e0b2
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-01 13:50:39 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -12,7 +12,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -S --noconfirm xorg xorg-xinit xclip
-pacman -S --noconfirm networkmanager neovim sudo python-pip base-devel os-prober grub vlc htop git wget
+pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget
 pacman -S --noconfirm npm # needed for neovim LSPs...
 
 systemctl enable NetworkManager
