e469e8cfdd1694c7a75e83b1ac805a0472706b30
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-01 13:52:05 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -12,7 +12,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -S --noconfirm xorg xorg-xinit xclip
-pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget
+pacman -S --noconfirm networkmanager helix sudo base-devel os-prober grub vlc htop git wget
 
 systemctl enable NetworkManager
 
