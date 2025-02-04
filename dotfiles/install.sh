ec3dd71bc0a014f5814057e6fb555e27b062326b
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-04 14:03:43 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/Connor-McCartney/deploy-arch-dwm
 
 
 mv /tmp/deploy-arch-dwm/dotfiles/.config/alacritty /home/connor/.config
-mv /tmp/deploy-arch-dwm/dotfiles/.tmux.conf /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.bashrc /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.wallpapers /home/connor
 
