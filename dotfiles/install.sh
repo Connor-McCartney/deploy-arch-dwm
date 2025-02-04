f1e2dab64672a9f673d4d4e50cd161f8eb542103
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-04 19:01:06 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -8,6 +8,6 @@ mv /tmp/deploy-arch-dwm/dotfiles/.config/alacritty /home/connor/.config
 mv /tmp/deploy-arch-dwm/dotfiles/.bashrc /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.wallpapers /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.switcher.sh /home/connor
-
+chmod +x /home/connor/.switcher.sh
 
 rm -rf /tmp/deploy-arch-dwm
