f61d8e83dfa0647ffd70d9d342634118d3e2ff00
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-05 01:33:12 +1000

    Update .switcher.sh

--- dotfiles/.switcher.sh
+++ dotfiles/.switcher.sh
@@ -6,10 +6,12 @@ xte 'key Escape' 'str :w' 'key Return'
 
 # switch
 xte 'keydown Super_L' 'keydown Tab' 'keyup Super_L' 'keyup Tab'
-sleep 0.1
 
 # clear
 xte 'keydown Control_L' 'keydown l' 'keyup Control_L' 'keyup l'
 
 # run
 xte 'key Up' 'key Return'
+
+# switch back
+xte 'keydown Super_L' 'keydown Tab' 'keyup Super_L' 'keyup Tab'
