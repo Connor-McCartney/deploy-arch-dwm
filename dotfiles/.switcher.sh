12f644a398d0e38847a6053dc011b59b30366542
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-04 18:07:56 +1000

    Create .switcher.sh

---
+++ dotfiles/.switcher.sh
@@ -0,0 +1,15 @@
+# turn off Windows key because that's what triggers this
+xte 'keyup Super_L'
+
+# save
+xte 'key Escape' 'str :w' 'key Return'
+
+# switch
+xte 'keydown Super_L' 'keydown Tab' 'keyup Super_L' 'keyup Tab'
+sleep 0.1
+
+# clear
+xte 'keydown Control_L' 'keydown l' 'keyup Control_L' 'keyup l'
+
+# run
+xte 'key Up' 'key Return'
