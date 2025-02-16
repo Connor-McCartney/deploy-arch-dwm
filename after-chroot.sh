6319acaa4ca86f3b467a21d040949d784c3bb969
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-02-16 12:04:53 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -1,10 +1,11 @@
 set -e
 
-#DISK="/dev/sda"
-DISK="/dev/vda"
+DISK="/dev/sda"
+#DISK="/dev/vda"
 
 
-ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+#ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
