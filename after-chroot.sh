5a6d1997a1ef66592e408363fe48714971bb6e07
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-03-04 09:24:40 +0900

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -13,7 +13,7 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 
 pacman -S --noconfirm xorg xorg-xinit xclip xautomation xorgproto
-pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd
+pacman -S --noconfirm networkmanager neovim sudo base-devel os-prober grub vlc htop git wget ripgrep fzf bat fd konsole
 pacman -S --noconfirm alsa-utils # adjust with alsamixer, arrowkeys and m toggles mute
 systemctl enable NetworkManager
 
