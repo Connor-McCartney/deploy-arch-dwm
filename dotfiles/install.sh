10c5c7509a4b5f4775d9ba70744259e1e626435b
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-03-19 01:31:53 +0900

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -12,6 +12,7 @@ mv /tmp/deploy-arch-dwm/dotfiles/.bashrc /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/pyproject.toml /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.wallpapers /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.switcher.sh /home/connor
+mv /tmp/deploy-arch-dwm/dotfiles/.xsage.sh /home/connor
 chmod +x /home/connor/.switcher.sh
 
 rm -rf /tmp/deploy-arch-dwm
