78d9020fbacc2b41d58da2589699b1c03411b915
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-03-28 04:24:23 +0900

    Create neoscroll.lua

---
+++ dotfiles/.config/nvim/lua/connor/neoscroll.lua
@@ -0,0 +1,8 @@
+require('neoscroll').setup({
+  mappings = {'<C-u>', '<C-d>'},
+  duration_multiplier = 0.5,
+  hide_cursor = false,
+  stop_eof = true,
+  respect_scrolloff = true,
+  cursor_scrolls_alone = false,
+})
