4979cce881e549c8256bb5cd1435d7b848acdadd
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-04-30 17:39:46 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -10,6 +10,7 @@ mv /tmp/deploy-arch-dwm/dotfiles/.local/share/konsole /home/connor/.local/share
 mv /tmp/deploy-arch-dwm/dotfiles/.config/nvim /home/connor/.config
 mv /tmp/deploy-arch-dwm/dotfiles/.config/picom /home/connor/.config
 mv /tmp/deploy-arch-dwm/dotfiles/.bashrc /home/connor
+mv /tmp/deploy-arch-dwm/dotfiles/.fdignore /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/pyproject.toml /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.wallpapers /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.switcher.sh /home/connor
