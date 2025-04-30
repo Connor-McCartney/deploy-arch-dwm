11a1893cbd54273678785c1ec1c0bb76efc3e43d
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-04-30 16:57:08 +1000

    Update init.lua

--- dotfiles/.config/nvim/lua/connor/init.lua
+++ dotfiles/.config/nvim/lua/connor/init.lua
@@ -2,6 +2,6 @@ require('connor.set')
 require('connor.packer')
 require('connor.remap')
 require('connor.treesitter')
-require('connor.smearcursor')
+--require('connor.smearcursor')
 require('connor.neoscroll')
 require("better_escape").setup{}
