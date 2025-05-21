57f297b10fa3b86c1737d69602825034f3bf3a7a
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-05-21 14:50:36 +1000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -18,6 +18,12 @@ mv /tmp/deploy-arch-dwm/dotfiles/.switcher.sh /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.xsage.sh /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.vim_quit.sh /home/connor
 mv /tmp/deploy-arch-dwm/dotfiles/.vim_saver.sh /home/connor
-chmod +x /home/connor/.switcher.sh /home/connor/.vim_quit.sh /home/connor/.vim_saver.sh /home/connor/.xsage.sh
+mv /tmp/deploy-arch-dwm/dotfiles/.upvol.sh /home/connor
+mv /tmp/deploy-arch-dwm/dotfiles/.downvol.sh /home/connor
+
+
+mv /tmp/deploy-arch-dwm/dotfiles/.volume_change.mp3 /home/connor
+
+chmod +x /home/connor/.switcher.sh /home/connor/.vim_quit.sh /home/connor/.vim_saver.sh /home/connor/.xsage.sh /home/connor/.upvol.sh /home/connor/.downvol.sh
 
 rm -rf /tmp/deploy-arch-dwm
