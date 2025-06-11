5a63a89061b6b797f7c2e85ecfa6d77eb4ef6bc5
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-06-11 23:06:28 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -40,7 +40,7 @@ sed -i 's/'#autologin-user='/'autologin-user=connor'/g' /etc/lightdm/lightdm.con
 sed -i 's/'#autologin-session='/'autologin-session=dwm'/g' /etc/lightdm/lightdm.conf
 
 
-useradd -m -G users,wheel,audio,video,autologin -s /bin/bash connor
+useradd -m -G users,wheel,audio,video,autologin,libvirt -s /bin/bash connor
 printf " \n \n" | passwd
 printf " \n \n" | passwd connor
 echo -e "root ALL=(ALL:ALL) ALL\n%wheel ALL=(ALL:ALL) NOPASSWD: ALL\n@includedir /etc/sudoers.d" > /etc/sudoers
