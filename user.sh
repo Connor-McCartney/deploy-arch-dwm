21c4ef83341eb05f208c45f3038451bccc657488
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-06-25 06:54:45 +1000

    Update user.sh

--- user.sh
+++ user.sh
@@ -16,6 +16,7 @@ mv /tmp/deploy-arch-dwm/suckless /home/connor
 cd /home/connor/suckless/dwm && sudo make clean install
 cd /home/connor/suckless/dmenu && sudo make clean install
 cd /home/connor/suckless/slstatus && sudo make clean install
+cd /home/connor/suckless/bongocat && sudo make install
 rm -rf /tmp/deploy-arch-dwm
 
 # dotfiles
