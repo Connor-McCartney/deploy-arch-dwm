100b2070277709fde4344acb59d2f336d640f0fc
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-03 13:21:13 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,32 +13,32 @@ set -e
 #swapon "$DISK""1"
 
 # 2. UEFI
-DISK="/dev/nvme0n1"
-printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n+16G\nt\n2\n19\nn\n3\n\n\nw\n" | fdisk $DISK  
-mkfs.vfat -F 32 "$DISK""p1"
-mkswap "$DISK""p2"
-mkfs.ext4 "$DISK""p3"
-mount "$DISK""p3" /mnt # mount root first?!
-swapon "$DISK""p2"
-mkdir /mnt/boot
-mount "$DISK""p1" /mnt/boot
-
-# 3. UEFI + LUKS
 #DISK="/dev/nvme0n1"
-#printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n\nw\n" | fdisk $DISK  
-#cryptsetup luksFormat "$DISK""p2"
-#cryptsetup open "$DISK""p2" cryptlvm
-#pvcreate /dev/mapper/cryptlvm
-#vgcreate vg1 /dev/mapper/cryptlvm
-#lvcreate -L 8G vg1 -n swap
-#lvcreate -l 100%FREE vg1 -n root
+#printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n+16G\nt\n2\n19\nn\n3\n\n\nw\n" | fdisk $DISK  
 #mkfs.vfat -F 32 "$DISK""p1"
-#mkfs.ext4 /dev/vg1/root
-#mkswap /dev/vg1/swap
-#mount /dev/vg1/root /mnt
+#mkswap "$DISK""p2"
+#mkfs.ext4 "$DISK""p3"
+#mount "$DISK""p3" /mnt # mount root first!!
+#swapon "$DISK""p2"
 #mkdir /mnt/boot
 #mount "$DISK""p1" /mnt/boot
-#swapon /dev/vg1/swap
+
+# 3. UEFI + LUKS
+DISK="/dev/nvme0n1"
+printf "g\nn\n1\n\n+256M\nt\n1\nn\n2\n\n\nw\n" | fdisk $DISK  
+cryptsetup luksFormat "$DISK""p2"
+cryptsetup open "$DISK""p2" cryptlvm
+pvcreate /dev/mapper/cryptlvm
+vgcreate vg1 /dev/mapper/cryptlvm
+lvcreate -L 8G vg1 -n swap
+lvcreate -l 100%FREE vg1 -n root
+mkfs.vfat -F 32 "$DISK""p1"
+mkfs.ext4 /dev/vg1/root
+mkswap /dev/vg1/swap
+mount /dev/vg1/root /mnt
+mkdir /mnt/boot
+mount "$DISK""p1" /mnt/boot
+swapon /dev/vg1/swap
 
 
 pacstrap -K /mnt base linux linux-firmware
