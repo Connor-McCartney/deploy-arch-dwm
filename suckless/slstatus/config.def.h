347bff9b12d9dc914631bb48bd9dc7ba72a8d0fc
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-07 21:12:21 +1000

    Update config.def.h

--- suckless/slstatus/config.def.h
+++ suckless/slstatus/config.def.h
@@ -68,12 +68,12 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format          argument */
 
- 	{ cpu_perc,      "  CPU %s%%",       NULL },
+ 	{ cpu_perc,      "   CPU %s%%",       NULL },
  	{ ram_perc,      "  ｜  RAM %s%%",     NULL },
  	{ temp,          "  ｜  %s°C",         "/sys/class/thermal/thermal_zone0/temp" }, 
  	{ vol_perc,      "  ｜  %s%%",         "Master" }, 
  	{ wifi_perc,     "  ｜    %s%%",      "wlp3s0" }, 
  	{ battery_perc,  "  ｜    %s%%",      "BAT0" }, //  /sys/class/power_supply
  	{ battery_state, " %s",             "BAT0" }, //  /sys/class/power_supply
-	{ datetime,      "  ｜  %s",           "%a, %b %d  ｜  %I:%M:%S %p  " },
+	{ datetime,      "  ｜  %s",           "%a, %b %d  ｜  %I:%M:%S %p   " },
 };
