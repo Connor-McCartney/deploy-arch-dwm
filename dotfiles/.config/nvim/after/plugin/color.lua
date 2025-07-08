b744aa66d344d1560adf619635fff1560d7e7ddf
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-07-09 07:57:29 +1000

    Update color.lua

--- dotfiles/.config/nvim/after/plugin/color.lua
+++ dotfiles/.config/nvim/after/plugin/color.lua
@@ -1,9 +1,3 @@
-vim.g.sonokai_transparent_background = true
-vim.g.sonokai_disable_italic_comment = true
-vim.cmd("colorscheme sonokai")
-
-
-
 local function Round(x)
     return math.floor(x+0.5)
 end
@@ -20,12 +14,29 @@ local function HSV2RGB (h, s, v)
     return string.format("#%02x%02x%02x", r, g, b)
 end
 
+
 vim.cmd("let g:sonokai_colors_override = {" ..
 --string.format("'grey':   ['%s', '0'],",   HSV2RGB(270, 0.40, 1.0)) .. -- comments
-string.format("'purple': ['%s', '0'],",   HSV2RGB(314, 0.40, 1.0)) .. -- numbers
-string.format("'yellow': ['%s', '0'],",   HSV2RGB(194, 0.30, 1.0)) .. -- strings
-string.format("'orange': ['%s', '0'],",   HSV2RGB(218, 0.40, 1.0)) .. -- struct/class vars
-string.format("'blue':   ['%s', '0'],",   HSV2RGB(242, 0.40, 1.0)) .. -- packages
-string.format("'green':  ['%s', '0'],",   HSV2RGB(314, 0.50, 1.0)) .. -- function names
-string.format("'fg':     ['%s', '0'],",   HSV2RGB(290, 0.35, 1.0)) .. -- variables
-string.format("'red':    ['%s', '250']}", HSV2RGB(266, 0.40, 1.0)))   -- keywords
+string.format("'purple': ['%s', '0'],",   HSV2RGB( 50, 0.50, 1.0)) .. -- numbers
+string.format("'yellow': ['%s', '0'],",   HSV2RGB(100, 0.50, 1.0)) .. -- strings
+string.format("'orange': ['%s', '0'],",   HSV2RGB(150, 0.50, 1.0)) .. -- struct/class vars
+string.format("'blue':   ['%s', '0'],",   HSV2RGB(200, 0.50, 1.0)) .. -- packages
+string.format("'green':  ['%s', '0'],",   HSV2RGB(250, 0.50, 1.0)) .. -- function names
+string.format("'fg':     ['%s', '0'],",   HSV2RGB(300, 0.50, 1.0)) .. -- variables
+string.format("'red':    ['%s', '250']}", HSV2RGB(350, 0.50, 1.0)))   -- keywords
+
+--vim.cmd("let g:sonokai_colors_override = {" ..
+----string.format("'grey':   ['%s', '0'],",   HSV2RGB(270, 0.40, 1.0)) .. -- comments
+--string.format("'purple': ['%s', '0'],",   HSV2RGB(314, 0.40, 1.0)) .. -- numbers
+--string.format("'yellow': ['%s', '0'],",   HSV2RGB(194, 0.30, 1.0)) .. -- strings
+--string.format("'orange': ['%s', '0'],",   HSV2RGB(218, 0.40, 1.0)) .. -- struct/class vars
+--string.format("'blue':   ['%s', '0'],",   HSV2RGB(242, 0.40, 1.0)) .. -- packages
+--string.format("'green':  ['%s', '0'],",   HSV2RGB(314, 0.50, 1.0)) .. -- function names
+--string.format("'fg':     ['%s', '0'],",   HSV2RGB(290, 0.35, 1.0)) .. -- variables
+--string.format("'red':    ['%s', '250']}", HSV2RGB(266, 0.40, 1.0)))   -- keywords
+
+
+-- https://github.com/sainnhe/sonokai/blob/master/doc/sonokai.txt
+vim.g.sonokai_transparent_background = true
+vim.g.sonokai_disable_italic_comment = true
+vim.cmd("colorscheme sonokai")
