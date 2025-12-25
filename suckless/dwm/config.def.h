d8b01554902fd80d12d14502949ec99c0af23e50
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-25 16:11:20 -0500

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -9,7 +9,7 @@ static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int attachbelow = 1;
-static const char *fonts[]          = { "monospace:size=10", "Hack Nerd Font Mono:size=10" };
+static const char *fonts[]          = { "ComicShannsMono Nerd Font Mono:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
