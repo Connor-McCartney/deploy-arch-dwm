ed86471d0f73235c0d5274ae90a035e39cf4ae2d
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-27 14:31:58 +1000

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -9,8 +9,7 @@ static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int attachbelow = 1;
-// NotoSansMono-Regular.ttf: "Noto Sans Mono" "Regular"
-static const char *fonts[]          = { "Noto Sans Mono:size=10", "ComicShannsMono Nerd Font Mono:size=11" };
+static const char *fonts[]          = { "Noto Sans Mono:size=10", "Hack Nerd Font:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
