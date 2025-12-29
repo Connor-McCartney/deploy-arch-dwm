dd4b4f1228e09386645d85a8457b05ee73a6f247
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2025-12-29 20:23:27 +1000

    Update build.sh

--- suckless/desktop_kirby/build.sh
+++ suckless/desktop_kirby/build.sh
@@ -1,3 +1,2 @@
 rm desktop_kirby
-gcc main.c -lm -lX11 -lXrender -lXtst -lXfixes -o desktop_kirby -Wall -Wextra -pedantic
-./desktop_kirby
+gcc main.c -O3 -lm -lX11 -lXrender -lXtst -lXfixes -o desktop_kirby -Wall -Wextra -pedantic
