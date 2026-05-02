367e0db4edd2dcd3879530f2c74023fad4544fb4
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2026-05-02 19:40:55 +1000

    Update after-chroot.sh

--- after-chroot.sh
+++ after-chroot.sh
@@ -1,7 +1,7 @@
 set -e
 
 DISK="/dev/vda"
-#DISK="/dev/sda"
+DISK="/dev/sda"
 
 
 ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
@@ -23,19 +23,19 @@ printf 'Section "Extensions"\n\tOption "DPMS" "false"\nEndSection\n' > /etc/X11/
 printf 'Section "ServerFlags"\n\tOption "BlankTime" "0"\nEndSection\n' > /etc/X11/xorg.conf.d/10-serverflags.conf
 
 # non-LUKS
-#printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
-##grub-install $DISK # BIOS
+printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+grub-install $DISK # BIOS
 #pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot # UEFI
-#grub-mkconfig -o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # LUKS UEFI
-printf "MODULES=()\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)\n" > /etc/mkinitcpio.conf
-pacman -S --noconfirm thin-provisioning-tools lvm2
-mkinitcpio -p linux
-pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+#printf "MODULES=()\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap consolefont block encrypt lvm2 filesystems fsck)\n" > /etc/mkinitcpio.conf
+#pacman -S --noconfirm thin-provisioning-tools lvm2
+#mkinitcpio -p linux
+#pacman -S efibootmgr --noconfirm; grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 uuid=$(blkid -s UUID -o value /dev/nvme0n1p2)
-printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog cryptdevice=UUID="$uuid":cryptlvm root=/dev/vg1/root nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+#printf "GRUB_TIMEOUT=1\nGRUB_DISTRIBUTOR=\"Arch\"\nGRUB_CMDLINE_LINUX=\"loglevel=1 nowatchdog cryptdevice=UUID="$uuid":cryptlvm root=/dev/vg1/root nvme_load=YES fsck.mode=skip modprobe.blacklist=iTCO_wdt\"\n" > /etc/default/grub
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 
 # lightdm auto-login
